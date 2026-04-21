b5ee1f10581e564893349d26a949ccbb0dff3a17
Mostafa El Bakhti <mel-bakh@c2r11p5.1337.ma>
2026-04-21 21:33:26 +0100

    push

--- py_twist_sequence.py
+++ py_twist_sequence.py
@@ -1,44 +1,57 @@
-def twister(nums, n):
-    if not nums:
-        return []
+# def twister(nums, n):
+#     if not nums:
+#         return []
     
-    tool = len(nums)
-    n = n % tool
+#     tool = len(nums)
+#     n = n % tool
 
-    return nums[-n:] + nums[:-n]
+#     return nums[-n:] + nums[:-n]
 
 
 
+def twister(nums , rot):
+    if rot == 0:
+        return nums
+    result = []
+    if rot > 0:
+        print(nums)
+        result = nums[-rot:]
+        nums = [nums.pop() for _ in range(rot)]
+        print(nums)
+    return result + nums
+        
+
+print(twister([1,2,3,4], 2))
 
 # Basic cases
-print(twister([1, 2, 3, 4, 5], 2))
-# [4, 5, 1, 2, 3]
+# print(twister([1, 2, 3, 4, 5], 2))
+# # [4, 5, 1, 2, 3]
 
-print(twister([4, 2, 1, -1, 'a'], 4))
-# [2, 1, -1, 'a', 4]
+# print(twister([4, 2, 1, -1, 'a'], 4))
+# # [2, 1, -1, 'a', 4]
 
-# Rotation equal to length
-print(twister([1, 2, 3], 3))
-# [1, 2, 3]
+# # Rotation equal to length
+# print(twister([1, 2, 3], 3))
+# # [1, 2, 3]
 
-# Rotation greater than length
-print(twister([1, 2, 3], 5))
-# [2, 3, 1]
+# # Rotation greater than length
+# print(twister([1, 2, 3], 5))
+# # [2, 3, 1]
 
-# 3 1 2
-# 2 3 1 
-# 1 2 3 
-# 3 1 2 
-# 2 3 1 
+# # 3 1 2
+# # 2 3 1 
+# # 1 2 3 
+# # 3 1 2 
+# # 2 3 1 
 
 
-# Negative rotation (left rotation)
-print(twister([1, 2, 3, 4], -1))
-# [2, 3, 4, 1]
+# # Negative rotation (left rotation)
+# print(twister([1, 2, 3, 4], -1))
+# # [2, 3, 4, 1]
 
-# Edge cases
-print(twister([] , 3))
-# []
+# # Edge cases
+# print(twister([] , 3))
+# # []
 
-print(twister([1], 10))
-# [1]
+# print(twister([1], 10))
+# # [1]
