d37c62f3f850e15d248e3671759289f34f0c663b
mostafa <mustmustapha67@gmail.com>
2026-06-22 15:35:46 +0100

    k

--- py_echo_validator.py
+++ py_echo_validator.py
@@ -1,9 +1,18 @@
 def echo_validator(text: str) -> bool:
-    cleaned = " "
-    for char in text:
-        if char.islpha():
+    cleaned = ""
+    for char in text.lower():
+        if char.isalpha():
             cleaned += char
 
+    if cleaned == "":
+        return False
+    # else:
+    return cleaned == cleaned[::-1]
 
 
-# echo_validator("A man a plan a canal Panama")
+
+
+print(echo_validator("A man a plan a canal Panama"))
+print(echo_validator("race a car"))
+print(echo_validator("Was it a car or a cat I saw"))
+print(echo_validator(""))
