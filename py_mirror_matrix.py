a97237031941dfb6f4cd5a5c973fb446da0794ed
Mostafa El Bakhti <mel-bakh@c1r7p3.1337.ma>
2026-06-23 22:15:25 +0100

    push

--- py_mirror_matrix.py
+++ py_mirror_matrix.py
@@ -1,13 +1,6 @@
 def mirror_matrix(matrix: list[list[int]]) -> list[list[int]]:
-    test = []
-    for x in matrix:
-        test += [x[::-1]]
-    return test
-            
-
-    
-
+    return [x[::-1] for x in matrix]
 
 print(mirror_matrix([[1,2,3],[4,5,6]]))
-mirror_matrix([[1,2],[3,4],[5,6]])
-mirror_matrix([[-1,-2],[-3,-4]])
+print(mirror_matrix([[1,2],[3,4],[5,6]]))
+print(mirror_matrix([[-1,-2],[-3,-4]]))
