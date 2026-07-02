cdef15805445546594eb245eb2172c2aadf28749
mostafa <mustmustapha67@gmail.com>
2026-07-02 01:40:04 +0100

    add

--- py_echo_validator.py
+++ py_echo_validator.py
@@ -1,18 +1,36 @@
+# def echo_validator(text: str) -> bool:
+#     cleaned = ""
+
+#     for char in text:
+#         if char.isalpha():
+#             cleaned += char
+
+#     if cleaned == "":
+#         return False
+
+#     return cleaned.lower() == cleaned[::-1].lower()
+    
+
+
+
+
+# print(echo_validator("A man a plan a canal Panama"))
+# print(echo_validator("race a car"))
+# print(echo_validator("Was it a car or a cat I saw"))
+# print(echo_validator(""))
+
+
 def echo_validator(text: str) -> bool:
     cleaned = ""
 
     for char in text:
         if char.isalpha():
-            cleaned += char
+            cleaned += char.lower()
 
     if cleaned == "":
-        return False
+        return  False
 
-    return cleaned.lower() == cleaned[::-1].lower()
-    
-
-
-
+    return cleaned == cleaned[::-1]
 
 print(echo_validator("A man a plan a canal Panama"))
 print(echo_validator("race a car"))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,20 @@
+def echo_validator(text: str) -> bool:
+    cleaned = ""
+
+    for char in text:
+        if char.isalpha():
+            cleaned += char
+
+    if cleaned == "":
+        return False
+
+    return cleaned.lower() == cleaned[::-1].lower()
+    
+
+
+
+
+print(echo_validator("A man a plan a canal Panama"))
+print(echo_validator("race a car"))
+print(echo_validator("Was it a car or a cat I saw"))
+print(echo_validator(""))
