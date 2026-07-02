1c5820e0ee501b2e89ac375fda1138ef5430c8cb
mostafa <mustmustapha67@gmail.com>
2026-07-02 06:01:39 +0100

    add

--- ff/py_bracket_validator.py
+++ ff/py_bracket_validator.py
@@ -43,3 +43,21 @@ print(bracket_validator("{[]}"))              # True
 print(bracket_validator("hello(world)"))      # True
 print(bracket_validator("((())"))             # False
 print(bracket_validator(""))                  # True
+
+
+def bracket_validator(s: str) -> bool:
+    stack = []
+
+    pairs = {
+        ")" : "(",
+        "]" : "[",
+        "}" : "{"
+    }
+
+    for char in s:
+        if char in "([{":
+            stack.append(char)
+        elif char in pairs:
+            if not stack or stack.pop() != pairs[char]:
+                return False
+    return len(stack) == 0
--- py_cryptic_sorter.py
+++ py_cryptic_sorter.py
@@ -2,13 +2,13 @@ def count_vo(s : str):
     vowels = "aeiou"
     count = 0
 
-    for char in s:
+    for char in s.lower():
         if char in vowels:
             count += 1
     return count
 
 def cryptic_sorter(strings: list[str]) -> list[str]:
-    return sorted(strings, key=lambda x: (len(x), x.lower(), x , count_vo(x)))
+    return sorted(strings, key=lambda x: (len(x), x.lower(), count_vo(x)))
 
 
 print(cryptic_sorter(["apple","cat","banana","dog","elephant"]))
